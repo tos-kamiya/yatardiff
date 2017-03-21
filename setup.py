2e3711002936dae4102a016a2b1249ee8780ee57
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2017-03-21 09:44:51 +0900

    now fallback to tar when archivemount is not installed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = [L for L in f.read().splitlines() if not L.startswith('#')]
 
-version = '0.3.1'
+version = '0.3.2'
 
 setup(
     name='yatardiff',
